d19d8052306cb0ac1d585a262f88c343a2b18044
tntgoku <130745911+tntgoku@users.noreply.github.com>
2023-05-13 14:57:33 +0700

    Phương trình bậc 4 trùng phương .cpp

--- bac44.cpp
+++ bac44.cpp
@@ -8,7 +8,7 @@ void bac4(float heso1,float heso2,float heso3 ){
 	float detal;
 		do{
 	printf("Nhập hệ số a,b,c vào: ");scanf("%f%f%f",&heso1,&heso2,&heso3);
-			\\Do phương trình ax^4+bx^2+c=0  đúng khi và chỉ khi có hệ số a khác 0
+			//Do phương trình ax^4+bx^2+c=0  đúng khi và chỉ khi có hệ số a khác 0
 	if(heso1==0){
 		printf("Số a phải khác 0\n");
 	}
@@ -33,17 +33,17 @@ void bac4(float heso1,float heso2,float heso3 ){
 			  x3=sqrt(abs(t2));
 			  x4=-x3;
 		if(t1>0 && t2<0){
-			\\ t2<0 không phải là nghiệm của phương trình vì t>=0
+			// t2<0 không phải là nghiệm của phương trình vì t>=0
 			printf("Phương trình có 2 nghiệm: %2.3f, %2.3f\n",x1,x2);
 			printf("%2.3fi, %2.3fi\t đây là nghiệm phức(vô nghiệm)\n",x3,x4);
 			}
 		else if(t1<0 && t2>0){
-			\\t1 không phải là nghiệm của phương trình vì t>=0
+			//t1 không phải là nghiệm của phương trình vì t>=0
 			printf("Phương trình có 2 nghiệm:, %2.3f, %2.3f,\n",x3,x4);
 			printf("%2.3fi, %2.3fi\t đây là nghiệm phức(vô nghiệm)\n",x1,x2);
 		}
 		else if(t1<0 && t2<0 ){
-			\\ đây là vô nghiệm do t1 và t2 đều <0
+			// đây là vô nghiệm do t1 và t2 đều <0
 	 		printf("Đây là vô nghiệm do 2 nghiêm t1,t2 đều <0\n");
 	 		printf("Phương trình có 4 nghiệm(chứa số phức) là: %2.3fi, %2.3fi, %2.3fi, %2.3fi\n",x1,x2,x3,x4);
 			 }
